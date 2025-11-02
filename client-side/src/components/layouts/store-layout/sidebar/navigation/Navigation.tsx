1ebacb04bb72b26f432d9a51cf9b3ca116455f00
internethero00 <egormix1703@gmail.com>
2025-11-02 23:00:27 +0200

    + navigation

--- client-side/src/components/layouts/store-layout/sidebar/navigation/Navigation.tsx
+++ client-side/src/components/layouts/store-layout/sidebar/navigation/Navigation.tsx
@@ -1,3 +1,45 @@
+'use client'
+import { useParams } from 'next/navigation'
+import { IMenuItem } from '@/components/layouts/store-layout/sidebar/navigation/menu.interface'
+import { Album, BarChart, FolderKanban, PaintBucket, Settings } from 'lucide-react'
+import { STORE_URL } from '@/config/url.config'
+
 export function Navigation() {
+	const params = useParams<{storeId: string}>();
+
+	const routes : IMenuItem[] = [
+		{
+			icon: BarChart,
+			link: STORE_URL.home(params.storeId),
+			value: 'Statistics',
+		},
+		{
+			icon: FolderKanban,
+			link: STORE_URL.products(params.storeId),
+			value: 'Products',
+		},
+		{
+			icon: Album,
+			link: STORE_URL.categories(params.storeId),
+			value: 'Categories',
+		},
+		{
+			icon: PaintBucket,
+			link: STORE_URL.colors(params.storeId),
+			value: 'Colors',
+		},
+		{
+			icon: PaintBucket,
+			link: STORE_URL.reviews(params.storeId),
+			value: 'Reviews',
+		},
+		{
+			icon: Settings,
+			link: STORE_URL.settings(params.storeId),
+			value: 'Settings',
+		},
+
+	]
+
 	return <div>Navigation</div>
 }
---
+++ client-side/src/components/layouts/store-layout/sidebar/navigation/menu.interface.ts
@@ -0,0 +1,8 @@
+import { LucideIcon } from 'lucide-react'
+
+
+export interface IMenuItem {
+	icon: LucideIcon
+	value: string
+	link: string
+}
