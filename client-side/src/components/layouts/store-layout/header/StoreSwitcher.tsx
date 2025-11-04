e5652ba1ff150d7373bb46fe4214f30ed6087e62
internethero00 <egormix1703@gmail.com>
2025-11-04 21:10:05 +0200

    + create store

--- client-side/src/components/layouts/store-layout/header/StoreSwitcher.tsx
+++ client-side/src/components/layouts/store-layout/header/StoreSwitcher.tsx
@@ -1,9 +1,72 @@
+'use client'
 import { IStore } from '@/shared/types/store.interface'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { STORE_URL } from "@/config/url.config";
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/Popover'
+import { Button } from '@/components/ui/Button'
+import { Plus, StoreIcon } from 'lucide-react'
+import { ChevronsUpDownIcon } from 'lucide-react'
+import {
+	Command,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList,
+	CommandSeparator
+} from '@/components/ui/Command'
+import { CreateStoreModal } from '@/components/ui/modals/CreateStoreModal'
 
 interface StoreSwitcherProps {
 	items: IStore[]
 }
 
 export function StoreSwitcher({items}: StoreSwitcherProps) {
-	return <>StoreSwitcher</>
+	const router = useRouter()
+	const [isOpen, setIsOpen] = useState(false)
+	const onStoreSelect = (storeId: string) => {
+		setIsOpen(false)
+		router.push(STORE_URL.home(storeId))
+	}
+	return (
+    <Popover open={isOpen} onOpenChange={setIsOpen}>
+      <PopoverTrigger asChild>
+        <Button variant='outline' size='sm' aria-expanded={isOpen}
+				role='combobox' aria-label='Shoose store' className='w-52'>
+          <StoreIcon className='mr-2 size-4 shrink-0 opacity-50'/>
+          Current store
+          <ChevronsUpDownIcon className='ml-auto size-4 shrink-0 opacity-50'/>
+        </Button>
+      </PopoverTrigger>
+		<PopoverContent className='w-52 p-0'>
+			<Command>
+				<CommandList>
+					<CommandInput placeholder="To search store..."/>
+					<CommandEmpty>No results found.</CommandEmpty>
+					<CommandGroup heading="Shops">
+						{items.map(store => (
+							<CommandItem key={store.id} onSelect={() => onStoreSelect(store.id)}
+							className='text-sm'>
+								<StoreIcon className='mr-2 size-4'/>
+								<div className='line-clamp-1'>{store.title}</div>
+							</CommandItem>
+						))}
+					</CommandGroup>
+				</CommandList>
+				<CommandSeparator />
+				<CommandList>
+					<CommandGroup>
+						<CreateStoreModal>
+							<CommandItem>
+								<Plus className='mr-2 size-4'/>
+								Create new store
+							</CommandItem>
+						</CreateStoreModal>
+					</CommandGroup>
+				</CommandList>
+			</Command>
+		</PopoverContent>
+    </Popover>
+  );
 }
---
+++ client-side/src/components/ui/modals/CreateStoreModal.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { PropsWithChildren, useState } from "react";
+import { useCreateStore } from "@/hooks/queries/stores/useCreateStore";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { IStoreCreate } from "@/shared/types/store.interface";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/Dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../form-elements/Form";
+import { Input } from "@/components/ui/form-elements/Input";
+import { Button } from '@/components/ui/Button'
+
+export function CreateStoreModal({ children }: PropsWithChildren<unknown>) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { createStore, isLoadingCreate } = useCreateStore();
+
+  const form = useForm<IStoreCreate>({
+    mode: "onChange",
+  });
+
+  const onSubmit: SubmitHandler<IStoreCreate> = (data) => {
+    createStore(data);
+    setIsOpen(false);
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger className='w-full'>{children}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>To create store</DialogTitle>
+          <DialogDescription>Please, add store name</DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="title"
+              rules={{ required: "The title is required" }}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Store name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Title"
+                      type="email"
+                      disabled={isLoadingCreate}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className='flex justify-end'>
+              <Button className='cursor-pointer' variant='primary' disabled={isLoadingCreate}>To Create</Button>
+            </div>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
