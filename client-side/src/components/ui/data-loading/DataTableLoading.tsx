4faf7993bd7ef90f3ba9f2cc6caf00fcdc0e6f90
internethero00 <egormix1703@gmail.com>
2025-11-09 22:51:57 +0200

    + DataTable

---
+++ client-side/src/components/ui/data-loading/DataTableLoading.tsx
@@ -0,0 +1,20 @@
+import styles from './DataTable.module.scss'
+import { Skeleton } from '@/components/ui/Skeleton'
+import { Card, CardContent } from '@/components/ui/Card'
+import { Loader } from '@/components/ui/Loader'
+
+export function DataTableLoading(){
+	return (
+		<div className={styles.loading}>
+			<Skeleton className={styles.heading}/>
+			<Skeleton className={styles.search}/>
+			<Card className={styles.table}>
+				<CardContent>
+					<div className={styles.loader_wrapper}>
+						<Loader />
+					</div>
+				</CardContent>
+			</Card>
+		</div>
+	)
+}
