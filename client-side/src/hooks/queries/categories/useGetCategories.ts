cfc4ecb7d6d02004361f3c83cbf659c716e54519
internethero00 <egormix1703@gmail.com>
2025-11-14 23:24:23 +0200

    + hook useGetCategories.ts

---
+++ client-side/src/hooks/queries/categories/useGetCategories.ts
@@ -0,0 +1,15 @@
+import { useParams } from 'next/navigation'
+import { useQuery } from '@tanstack/react-query'
+import { useMemo } from 'react'
+import { categoryService } from '@/services/category.service'
+
+export const useGetCategories = () => {
+	const params = useParams<{storeId: string}>();
+
+	const {data: categories, isLoading} = useQuery({
+		queryKey: ['get categories for store dashboard'],
+		queryFn: () => categoryService.getByStoreId(params.storeId),
+	})
+
+	return useMemo(() => ({categories, isLoading}), [categories, isLoading])
+}
