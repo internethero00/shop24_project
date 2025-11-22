c9b42869021b664f71090012c1156b55f600a3de
internethero00 <egormix1703@gmail.com>
2025-11-22 21:40:20 +0200

    + productCard update

--- client-side/src/components/ui/catalog/product-card/ProductCard.tsx
+++ client-side/src/components/ui/catalog/product-card/ProductCard.tsx
@@ -1,4 +1,9 @@
 import { IProduct } from '@/shared/types/product.interface'
+import styles from './ProductCard.module.css'
+import Link from 'next/link'
+import { PUBLIC_URL } from '@/config/url.config'
+import Image from 'next/image'
+import { formatPrice } from '@/utils/string/format-price'
 
 interface ProductCardProps{
 	product: IProduct;
@@ -6,8 +11,24 @@ interface ProductCardProps{
 
 export function ProductCard({product}: ProductCardProps) {
   return (
-    <div>
-		{product.title}
+    <div className={styles.card}>
+		<Link href={PUBLIC_URL.product(product.id)}>
+			<div className={styles.imageWrapper}>
+				<Image
+					src={product.images[0]}
+					alt={product.title}
+					fill
+					className={styles.image}
+				/>
+			</div>
+		</Link>
+		<h3 className={styles.title}>{product.title}</h3>
+		<Link
+			className={styles.category}
+			href={PUBLIC_URL.category(product.category.id)}>
+			{product.category.title}
+		</Link>
+		<p className={styles.price}>{formatPrice(product.price)}</p>
     </div>
   )
 }
