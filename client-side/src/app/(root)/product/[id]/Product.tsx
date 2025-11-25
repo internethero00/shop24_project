9d6920230327f277ee92000b7fa579b0f900cf23
internethero00 <egormix1703@gmail.com>
2025-11-25 22:21:14 +0200

    + product gallery page

---
+++ client-side/src/app/(root)/product/[id]/Product.tsx
@@ -0,0 +1,36 @@
+'use client'
+import { IProduct } from '@/shared/types/product.interface'
+import { useQuery } from '@tanstack/react-query'
+import { productService } from '@/services/product.service'
+import styles from './Product.module.css'
+import { Catalog } from '@/components/ui/catalog/Catalog'
+import { ProductGallery } from '@/app/(root)/product/[id]/product-gallery/ProductGallery'
+import { ProductInfo } from '@/app/(root)/product/[id]/product-info/ProductInfo'
+import { ProductReviews } from '@/app/(root)/product/[id]/product-reviews/ProductReviews'
+
+interface ProductProps {
+	initialProduct: IProduct,
+	similarProducts: IProduct[],
+	id?: string
+}
+
+export function Product({similarProducts, initialProduct, id=''}: ProductProps) {
+	const {data: product} = useQuery({
+		queryKey: ['product', initialProduct.id],
+		queryFn: () => productService.getById(initialProduct.id),
+		initialData: initialProduct,
+		enabled: !!id
+	})
+  return (
+    <div className={styles.product_page}>
+      <div className={styles.content}>
+		  <div className={styles.blocks}>
+			  <ProductGallery product={product}/>
+			  <ProductInfo product={product}/>
+		  </div>
+	  </div>
+		<Catalog title='Similar products' products={similarProducts}/>
+		<ProductReviews product={product}/>
+    </div>
+  )
+}
---
+++ client-side/src/app/(root)/product/[id]/page.tsx
@@ -0,0 +1,54 @@
+import { productService } from '@/services/product.service'
+import type { Metadata } from 'next'
+import { Product } from '@/app/(root)/product/[id]/Product'
+import { notFound } from 'next/navigation'
+
+export const revalidate = 60;
+
+export async function generateStaticParams() {
+	const products = await productService.getAll()
+
+	const paths = products.map(product => ({
+		id: product.id
+	}))
+	return paths
+}
+
+async function getProducts(id: string) {
+	try {
+		const product = await productService.getById(id)
+		const similarProducts = await productService.getSimilar(id)
+		return {product, similarProducts}
+	}
+	catch (error) {
+		return notFound()
+	}
+}
+
+export async function generateMetadata({ params }: { params: Promise<{ id: string }>}): Promise<Metadata> {
+	const { id } = await params;
+	const { product} = await getProducts(id);
+	return {
+		title: product.title,
+		description: product.description,
+		openGraph: {
+			images: [
+				{
+					url: product.images[0],
+					width: 1000,
+					height: 1000,
+					alt: product.title,
+				},
+			],
+		},
+	};
+}
+
+export default async function ProductPage({ params }: { params: Promise<{ id: string }>}) {
+	const { id } = await params;
+	const { product, similarProducts } = await getProducts(id);
+
+	return (
+		<Product initialProduct={product} similarProducts={similarProducts} id={id} />
+	);
+}
