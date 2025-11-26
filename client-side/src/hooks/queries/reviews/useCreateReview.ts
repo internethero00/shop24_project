b8274479b18618db30a16558057afc35fd758d1f
internethero00 <egormix1703@gmail.com>
2025-11-26 22:35:28 +0200

    + hook useCreateReview

---
+++ client-side/src/hooks/queries/reviews/useCreateReview.ts
@@ -0,0 +1,29 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { reviewService } from '@/services/review.service'
+import { IReviewInput } from '@/shared/types/review.interface'
+import toast from 'react-hot-toast'
+import { useParams } from 'next/navigation'
+import { useMemo } from 'react'
+
+export const useCreateReview = (storeId: string) => {
+	const params = useParams<{id: string}>()
+	const queryClient = useQueryClient()
+
+	const {mutate: createReview, isPending: isLoadingCreate} = useMutation({
+		mutationKey:['create review'],
+		mutationFn: (data: IReviewInput) =>
+			reviewService.create(data, params.id, storeId),
+		onSuccess(){
+			queryClient.invalidateQueries({
+				queryKey:['product']
+			})
+			toast.success('Review created successfully.')
+		},
+		onError(){
+			toast.error('Error while creating review')
+		}
+	})
+
+	return useMemo(() => ({createReview, isLoadingCreate}),[createReview, isLoadingCreate])
+
+}
